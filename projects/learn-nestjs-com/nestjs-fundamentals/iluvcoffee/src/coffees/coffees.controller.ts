0418d66e1607b9b6ded52aca5af3f289c05b097e
akira <33633391+hskwakr@users.noreply.github.com>
2024-07-14 18:27:05 +0900

    2-15

--- projects/learn-nestjs-com/nestjs-fundamentals/iluvcoffee/src/coffees/coffees.controller.ts
+++ projects/learn-nestjs-com/nestjs-fundamentals/iluvcoffee/src/coffees/coffees.controller.ts
@@ -25,13 +25,15 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coffeesService.findOne(id);
+  findOne(@Param('id') id: number) {
+    console.log(typeof id);
+    return this.coffeesService.findOne('' + id);
   }
 
   @HttpCode(HttpStatus.GONE)
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
     return this.coffeesService.create(createCoffeeDto);
   }
 
