294629a5a17b7db21c0ee295b024a1a5f423a114
akira <33633391+hskwakr@users.noreply.github.com>
2024-08-01 22:10:41 +0900

    4-5

--- projects/learn-nestjs-com/nestjs-fundamentals/iluvcoffee/src/coffees/coffees.module.ts
+++ projects/learn-nestjs-com/nestjs-fundamentals/iluvcoffee/src/coffees/coffees.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
 class MockCoffeeService {}
 
@@ -12,7 +13,10 @@ class MockCoffeeService {}
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   // providers: [{ provide: CoffeesService, useValue: new MockCoffeeService() }],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
