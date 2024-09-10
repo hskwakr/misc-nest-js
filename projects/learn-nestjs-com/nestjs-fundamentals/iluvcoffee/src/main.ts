2f1f3ec39c8d3b481019c70a8a54484059778b15
akira <33633391+hskwakr@users.noreply.github.com>
2024-09-10 20:59:04 +0900

    6-3

--- projects/learn-nestjs-com/nestjs-fundamentals/iluvcoffee/src/main.ts
+++ projects/learn-nestjs-com/nestjs-fundamentals/iluvcoffee/src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-    transformOptions : {
-      enableImplicitConversion: true
-    }
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
