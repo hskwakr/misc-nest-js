d952858af2edb4734c4a4971df47a1feda3bcd4c
akira <33633391+hskwakr@users.noreply.github.com>
2024-09-11 01:51:56 +0900

    6-5

--- projects/learn-nestjs-com/nestjs-fundamentals/iluvcoffee/src/coffees/coffees.controller.ts
+++ projects/learn-nestjs-com/nestjs-fundamentals/iluvcoffee/src/coffees/coffees.controller.ts
@@ -13,11 +13,13 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
+import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @Public()
   @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
     return this.coffeesService.findAll(paginationQuery);
@@ -36,7 +38,10 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto) {
+  update(
+    @Param('id') id: string,
+    @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto,
+  ) {
     return this.coffeesService.update(id, updateCoffeeDto);
   }
 
