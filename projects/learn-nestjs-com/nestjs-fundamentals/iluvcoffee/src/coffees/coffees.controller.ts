f8ad0d1988836c58c9ba774d0b8eb4f05a42469e
akira <33633391+hskwakr@users.noreply.github.com>
2024-09-27 18:27:04 +0900

    7-5

--- projects/learn-nestjs-com/nestjs-fundamentals/iluvcoffee/src/coffees/coffees.controller.ts
+++ projects/learn-nestjs-com/nestjs-fundamentals/iluvcoffee/src/coffees/coffees.controller.ts
@@ -14,7 +14,9 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
