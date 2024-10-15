0fe92d16e0f043528d9d3017ad1529d624fdf89a
akira <33633391+hskwakr@users.noreply.github.com>
2024-10-15 22:48:02 +0900

    8-6

--- projects/learn-nestjs-com/nestjs-fundamentals/iluvcoffee/test/coffee/coffees.e2e-spec.ts
+++ projects/learn-nestjs-com/nestjs-fundamentals/iluvcoffee/test/coffee/coffees.e2e-spec.ts
@@ -1,10 +1,24 @@
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesModule } from 'src/coffees/coffees.module';
+import { CreateCoffeeDto } from 'src/coffees/dto/create-coffee.dto';
+import * as request from 'supertest';
 
 describe('[Feature] Coffees - /coffees', () => {
+  const coffee = {
+    name: 'My Coffee',
+    brand: 'Buddy Brew',
+    flavors: ['chocolate', 'vanilla'],
+  };
+  const expectedPartialCoffee = expect.objectContaining({
+    ...coffee,
+    flavors: expect.arrayContaining(
+      coffee.flavors.map((name) => expect.objectContaining({ name })),
+    ),
+  });
   let app: INestApplication;
+  let httpServer;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -24,11 +38,42 @@ describe('[Feature] Coffees - /coffees', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+
     await app.init();
+    httpServer = app.getHttpServer();
   });
 
-  it.todo('Create [POST /]');
-  it.todo('Get all [GET /]');
+  it('Create [POST /]', async () => {
+    return await request(httpServer)
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED)
+      .then(({ body }) => {
+        expect(body).toEqual(expectedPartialCoffee);
+      });
+  });
+
+  // it('Get all [GET /]', async () => {
+  //   return await request(httpServer)
+  //     .get('/coffees')
+  //     .expect(HttpStatus.OK)
+  //     .then(({ body }) => {
+  //       // console.log(body);
+  //       expect(Array.isArray(body)).toBe(true);
+  //       // expect(body[0]).toEqual(expectedPartialCoffee);
+  //     });
+  // });
+
   it.todo('Get one [GET /:id]');
   it.todo('Update one [PUT /:id]');
   it.todo('Delete one [DELETE /:id]');
