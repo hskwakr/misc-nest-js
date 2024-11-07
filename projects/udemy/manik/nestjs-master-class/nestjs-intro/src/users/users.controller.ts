78c581d2fae8e1beebc1fe73119ff62df828c653
akira <33633391+hskwakr@users.noreply.github.com>
2024-11-07 19:47:33 +0900

    2-11

--- projects/udemy/manik/nestjs-master-class/nestjs-intro/src/users/users.controller.ts
+++ projects/udemy/manik/nestjs-master-class/nestjs-intro/src/users/users.controller.ts
@@ -1,17 +1,28 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Ip,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id/:optional?')
-  getUsers(@Param() params: any, @Query() query: any) {
-    console.log(params);
-    console.log(query);
+  getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log(id);
+    console.log(limit);
     return 'GET users endpoint';
   }
 
   @Post()
-  createUser(@Body() request: any) {
+  createUser(@Body() request: any, @Headers() headers: any, @Ip() ip: any) {
     console.log(request);
+    console.log(headers);
+    console.log(ip);
     return 'POST users endpoint';
   }
 }
