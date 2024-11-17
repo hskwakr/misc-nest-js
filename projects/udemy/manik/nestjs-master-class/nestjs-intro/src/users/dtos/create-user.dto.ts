bcd49e3aed796f0c710ff434bb26eb9fd4ea7bb2
akira <33633391+hskwakr@users.noreply.github.com>
2024-11-17 21:35:41 +0900

    3-6

---
+++ projects/udemy/manik/nestjs-master-class/nestjs-intro/src/users/dtos/create-user.dto.ts
@@ -0,0 +1,36 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(96)
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  @MaxLength(96)
+  lastName?: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+    message:
+      'Password must contain at least 8 characters, 1 uppercase letter, 1 lowercase letter and 1 number',
+  })
+  password: string;
+}
