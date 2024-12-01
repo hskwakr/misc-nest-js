d4c90b88b0e6f43df779522e39fd5d66b5c9e4e8
akira <33633391+hskwakr@users.noreply.github.com>
2024-12-02 08:55:44 +0900

    3-10

--- projects/udemy/manik/nestjs-master-class/nestjs-intro/src/users/users.controller.ts
+++ projects/udemy/manik/nestjs-master-class/nestjs-intro/src/users/users.controller.ts
@@ -9,18 +9,18 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id')
+  @Get('/:id?')
   getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe)
     limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(typeof id);
-    console.log(id);
+    console.log(getUsersParamDto);
     console.log(typeof limit);
     console.log(limit);
     console.log(typeof page);
