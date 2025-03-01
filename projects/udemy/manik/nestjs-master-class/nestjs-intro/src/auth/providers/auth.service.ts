8dd5d0e42cab23fad62b05e0b0d89eea6e6b6ee4
hskwakr <33633391+hskwakr@users.noreply.github.com>
2025-03-01 09:19:27 +0900

    4-14

--- projects/udemy/manik/nestjs-master-class/nestjs-intro/src/auth/providers/auth.service.ts
+++ projects/udemy/manik/nestjs-master-class/nestjs-intro/src/auth/providers/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(forwardRef(() => UsersService))
+    private readonly userService: UsersService,
+  ) {}
+
+  public login(email: string, password: string, id: string) {
+    const user = this.userService.findById('1234');
+
+    return 'SAMPLE_TOKEN';
+  }
+
+  public isAuth() {
+    return true;
+  }
+}
